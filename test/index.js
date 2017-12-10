30d07ca37d441e008e60caa080212c5e3f4c8ff6
Robin Millette <robin@millette.info>
2017-12-09 22:07:11 -0500

    Add tests.

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var test = require('tape')
-var enc = require('dat-encoding')
 var datResolve = require('..')
+var enc = require('dat-encoding')
 
 // Strings that do not require lookup
 var stringKeys = [
@@ -21,27 +21,38 @@ var stringBadKeys = [
 test('resolve key with path', function (t) {
   t.plan(2)
   datResolve('87ed2e3b160f261a032af03921a3bd09227d0a4cde73466c17114816cae43336/path', function (err, newKey) {
-    console.log('KEYPATH', err, newKey)
     t.notOk(err, 'not expected error')
     t.ok(newKey, 'is a key')
   })
 })
 
-/*
-test('resolve hostname with path', function (t) {
+test('resolve https hostname with path', function (t) {
   t.plan(2)
-  datResolve('beakerbrowser.com/path', function (err, newKey) {
-    console.log('HOSTNAMEPATH', err, newKey)
+  datResolve('https://beakerbrowser.com/path', function (err, newKey) {
     t.notOk(err, 'not expected error')
     t.ok(newKey, 'is a key')
   })
 })
-*/
+
+test('resolve dat hostname with path', function (t) {
+  t.plan(2)
+  datResolve('dat://beakerbrowser.com/path', function (err, newKey) {
+    t.notOk(err, 'not expected error')
+    t.ok(newKey, 'is a key')
+  })
+})
+
+test('resolve hostname with path', function (t) {
+  t.plan(2)
+  datResolve('beakerbrowser.com/path', function (err, newKey) {
+    t.notOk(err, 'not expected error')
+    t.ok(newKey, 'is a key')
+  })
+})
 
 test('resolve key with version', function (t) {
   t.plan(2)
   datResolve('87ed2e3b160f261a032af03921a3bd09227d0a4cde73466c17114816cae43336+5', function (err, newKey) {
-    console.log('KEYVERSION', err, newKey)
     t.notOk(err, 'not expected error')
     t.ok(newKey, 'is a key')
   })
@@ -50,7 +61,6 @@ test('resolve key with version', function (t) {
 test('resolve hostname with version', function (t) {
   t.plan(2)
   datResolve('beakerbrowser.com+5', function (err, newKey) {
-    console.log('HOSTNAMEVERSION', err, newKey)
     t.notOk(err, 'not expected error')
     t.ok(newKey, 'is a key')
   })
